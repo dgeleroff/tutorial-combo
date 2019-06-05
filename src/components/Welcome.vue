302c654f3fc7d989d488f5ba7f0faa63a5f6d769
Daniel Geleroff <dgeleroff@macmini-4435.ad.infotechfl.com>
2019-06-05 16:41:08 -0400

    disabled es lint added route for welcome and dice

---
+++ src/components/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="welcome">
+    <h2>Welcome!</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Welcome',
+    // data () {
+    //
+    // }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import DiceRoll from '@/components/DiceRoll.vue'
 import About from '@/components/About.vue'
+import Welcome from '@/components/Welcome.vue'
 
 Vue.use(Router)
 
@@ -9,13 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'DiceRoll',
-      component: DiceRoll
+      name: 'Welcome',
+      component: Welcome
     },
     {
       path: '/about',
       name: 'About',
       component: About
+    },
+    {
+      path: '/diceroll',
+      name: 'DiceRoll',
+      component: DiceRoll
     }
   ]
 })
